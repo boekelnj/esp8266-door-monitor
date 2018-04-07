69358e84cf5dd8633967235334bb1b3d71593395
boekelnj <boekelnj@gmail.com>
2018-04-07 18:12:38 -0500

    Add files via upload

---
+++ door-monitor.ino
@@ -0,0 +1,150 @@
+//  notbookies 8/5/17
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>  //used to connect to multiple networks
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoJson.h>  // used for cleaner JSON code/logic
+#include "index.h"  // the HTML/CSS/JavaScript is in this separate file, as a string
+
+#define DOOR_PIN 5
+#define LED_PIN 2
+#define NETWORK "********"
+#define PASSWD "********"
+
+ESP8266WiFiMulti WiFiMulti;  //create an instance of the ESP8266WiFiMulti class
+ESP8266WebServer server(80);    //listen on port 80
+
+StaticJsonBuffer<200> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
+
+char dataBuffer[256]; // string that will hold JSON object
+bool  lastState;
+int   visitors = 0;
+
+unsigned long start, finished = millis();
+unsigned long elapsed,
+              longestOpen, longestClosed,
+              avgOpenCount, avgClosedCount,
+              avgOpenTot, avgClosedTot,
+              avgOpen, avgClosed = 0;
+
+void handleWifi() {
+  
+  WiFiMulti.addAP(NETWORK, PASSWD);
+  Serial.println();
+  Serial.print("Wait for WiFi ...");
+  
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(500);
+  
+}
+
+void handleMDNS() {
+  
+  // mDNS service - to be able to resolve the name "doorsensor" in a browser
+  if (!MDNS.begin("doorsensor")) {
+    Serial.println("Error setting up MDNS responder!");
+    while(1) { 
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  MDNS.addService("http", "tcp", 80); // add service to MDNS
+
+}
+
+//  blink LED x number of times when system is ready
+void systemReady(int blinks) {
+  int dT = 300;
+  for (int x = 0; x < blinks; x++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(dT);
+    digitalWrite(LED_PIN, LOW);
+    delay(dT);
+  }
+}
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode     (DOOR_PIN,  INPUT);
+  pinMode     (LED_PIN,   OUTPUT);
+  digitalWrite(LED_PIN,   LOW);
+
+  lastState = digitalRead(DOOR_PIN);
+
+  handleWifi();
+  handleMDNS();
+  systemReady(3);
+  
+  // handle index and GET request
+  server.on ( "/",      []() {server.send ( 200, "text/html",         SITE_index ); });
+  server.on ( "/data",  []() {server.send ( 200, "application/json",  dataBuffer ); });
+  server.begin();
+
+}
+  
+void loop() {
+
+  if(WiFiMulti.run() != WL_CONNECTED){      // auto-reconnect (hopefully)
+    Serial.println("Wifi not connected!");
+    delay(1000);
+    handleWifi();
+  }
+  
+  bool doorState = digitalRead(DOOR_PIN);
+  
+  if (doorState != lastState){  // if state has changed
+    finished = millis();
+    elapsed = finished - start; // time since last change
+    root.prettyPrintTo(Serial);
+    start = millis();           // this needs to be after elapsed is calculated
+
+    if (doorState == 0){            // if door is open
+      digitalWrite(LED_PIN, LOW);
+      avgClosedCount ++;
+      avgClosedTot += elapsed;
+      avgClosed = avgClosedTot / avgClosedCount;
+      if (elapsed > longestClosed){
+        longestClosed = elapsed;    // set longestClosed based on time since last open
+      }
+    } else if (doorState == 1){     // if door is closed
+      digitalWrite(LED_PIN, HIGH);
+      avgOpenCount ++;
+      avgOpenTot += elapsed;
+      avgOpen = avgOpenTot / avgOpenCount;
+      if (elapsed > longestOpen){
+        longestOpen = elapsed;      // set longestOpen based on time since last open
+      }
+    }
+  }
+  
+  delay(100);   // poor man's debounce
+  // counting a visitor if the door goes from closed to open (full cycle)
+  if (doorState == 0 && lastState == 1){
+    visitors ++;
+  }
+
+  // add variables to JSON object
+  root["doorMsg"]       = doorState;
+  root["timer"]         = millis() - start;
+  root["visitors"]      = visitors;
+  root["longestOpen"]   = longestOpen;
+  root["longestClosed"] = longestClosed;
+  root["avgOpen"]       = avgOpen;
+  root["avgClosed"]     = avgClosed;
+  root.printTo(dataBuffer, sizeof(dataBuffer));   // send JSON object to string
+  
+  server.handleClient();  // dont know what this does
+  lastState = doorState;  // for determining if the state changes
+}
+
---
+++ index.h
@@ -0,0 +1,134 @@
+// notbookies 8/5/17
+// Contains HTML/CSS/JavaScript for door project website
+
+const char SITE_index[] PROGMEM = R"=====(
+
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8"/>
+            <meta name="viewport" content="width=device-width"/>
+            <link rel="shortcut icon" type="image/png" href="https://image.flaticon.com/icons/png/512/194/194432.png"/>
+            <title>Door Sensor</title>
+            <style>
+                /* local styles */
+                .opened     {color: green;}
+                .closed     {color: red;}
+                #door-state {font-size: 400%;}
+    
+                /* global styles */
+                body {
+                    font-family: Arial, Helvetica, sans-serif;
+                    font-size: 100%;
+                    background-color: #f4f4f4;
+                }
+                .container {
+                    width: 80%;
+                    margin: auto;
+                    overflow: hidden;
+                }
+                .column1 {float: left;}
+                .column2 {float: right;}
+                p{
+                    font-size: 200%;
+                    color: #696969;
+                }
+            </style>
+        </head>
+    
+        <body>
+            <div class="container">
+                <div align="center">
+                    <h1>The door is:</h1>
+                    <h2 id="door-state"></h2>
+                    <h1>And has been for:</h1>
+                    <p id="current-timer"></p>
+                    <h1>Number of visitors:</h1>
+                    <p id="visitors"></p>
+                </div>
+                <div align="center" class="column1">
+                    <h1>Occupancy record:</h1>
+                    <p id="closed-record"></p>
+                    <h1>Vacancy record:</h1>
+                    <p id="open-record"></p>
+                </div>
+                <div align="center" class="column2">
+                    <h1>Occupancy average:</h1>
+                    <p id="closed-avg"></p>
+                    <h1>Vacancy average:</h1>
+                    <p id="open-avg"></p>
+                </div>
+            </div>
+    
+        <script>
+            /* declaring variable to make logic below cleaner */
+            var doorState       = document.getElementById('door-state')
+              , currentTimer    = document.getElementById('current-timer')
+              , closedRecord    = document.getElementById('closed-record')
+              , openRecord      = document.getElementById('open-record')
+              , thumbs          = document.getElementById('thumbs')
+              , visitors        = document.getElementById('visitors')
+              , closedAvg       = document.getElementById('closed-avg')
+              , openAvg         = document.getElementById('open-avg');
+    
+            /* converts ms to hrs/min/sec */
+            function msFormat(duration) {
+                var seconds = parseInt( (duration / 1000)         % 60 )
+                , minutes   = parseInt( (duration / (1000*60))    % 60 )
+                , hours     = parseInt( (duration / (1000*60*60)) % 24 );
+    
+                /* multiple returns to remove hr/min if =0 */
+                if (hours != 0){
+                    return hours + " hr " + minutes + " min " + seconds + " sec";
+                } else if (minutes != 0){
+                    return minutes + " min " + seconds + " sec";
+                } else if (seconds != 0){
+                    return seconds + " sec";
+                } else {
+                    return 0 + " sec";
+                }
+            }
+            /* main 'loop' */
+            function updateData() {
+                console.log("update ran");
+                var xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function() {
+                    /* readyState 4: request finished and response is ready */
+                    /* status 200: "OK" */
+                    if (this.readyState == 4 && this.status == 200) {
+                        /* JSON.parse takes JSON string and constructs object(s) */
+                        var data = JSON.parse(this.responseText);
+                        console.log(data);
+                        
+                        currentTimer.innerHTML  = msFormat(data.timer);
+                        visitors.innerHTML      = data.visitors;
+                        openRecord.innerHTML    = msFormat(data.longestOpen);
+                        closedRecord.innerHTML  = msFormat(data.longestClosed);
+                        openAvg.innerHTML       = msFormat(data.avgOpen);
+                        closedAvg.innerHTML     = msFormat(data.avgClosed);
+    
+                        /* change door message text and color */
+                        if (data.doorMsg == false) {
+                            doorState.innerHTML = "OPEN";
+                            doorState.className = "opened";
+                        }
+                        if (data.doorMsg == true){
+                            doorState.innerHTML = "CLOSED";
+                            doorState.className = "closed";
+                        }
+                    }
+                };
+                /* GET request at "IP_address/data" (JSON string) */
+                xhttp.open("GET", "/data", true);
+                xhttp.send();
+            }
+            /* run update function once every 250ms */
+            updateData();
+            window.setInterval(updateData, 250);
+        </script>
+    
+        </body>
+    </html>
+
+)=====";
+
